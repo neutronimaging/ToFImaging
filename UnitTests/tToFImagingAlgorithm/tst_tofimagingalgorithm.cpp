cd43975a4940fd8299a538b886f1422c6877ae1a
chiaracarminati <chiara.carminati@psi.ch>
2019-11-12 17:25:06 +0100

    finished tests on lineshape

--- UnitTests/tToFImagingAlgorithm/tst_tofimagingalgorithm.cpp
+++ UnitTests/tToFImagingAlgorithm/tst_tofimagingalgorithm.cpp
@@ -376,33 +376,24 @@ void ToFImagingAlgorithm::test_GradientGaussian()
 
 void ToFImagingAlgorithm::test_AttenuationExp()
 {
+    double *param = new double[7];
+    double *expected_param = new double[7];
 
-// This is still to figure out
+    param[0] = 0.056568;
+    param[1] = 0.0001;
+    param[2] = 0.0015;
+    param[3] = 0.5854773;
+    param[4] = 8.245339;
+    param[5] = 0.6237067;
+    param[6] = -20.09876;
 
-}
-
-void ToFImagingAlgorithm::test_AttenuationLin()
-{
-
-    // This fails: to check
-    double *param = new double[7]; // initial parameters
-    param[0]=0.056568;
-    param[1]=0.0001;
-    param[2]=0.0015;
-    param[5]=-0.12174127292834003;
-    param[6]=31.65784345829804;
-    param[3]=0.3221873247378136;
-    param[4]=5.2674654703300545;
-
-
-    double *expected_params = new double[7];
-    expected_params[0] = 0.05788519;
-    expected_params[1] = 2.1650e-04;
-    expected_params[2] = 1.5581e-04;
-    expected_params[5] = 0.14975358;
-    expected_params[6] = 7.67236371;
-    expected_params[3] = 0.15162227;
-    expected_params[4] = 26.4337443;
+    expected_param[0] = 0.05788515;
+    expected_param[1] = 2.1647e-04;
+    expected_param[2] = 1.5583e-04;
+    expected_param[3] = 0.73414570;
+    expected_param[4] = 5.22685127;
+    expected_param[5] = 0.44088105;
+    expected_param[6] = -16.6111563;
 
     short loop=0; //short for loop for input
     string line; //this will contain the data read from the file
@@ -415,7 +406,99 @@ void ToFImagingAlgorithm::test_AttenuationLin()
     double *first_guess = new double[N];
     double *y = new double[N];
 
-    double eps=0.0001;
+    double eps=0.001;
+
+    for (double a; myfile>>a;)
+    {
+        x[loop]=a;
+        loop++;
+    }
+
+    ifstream myfile_y ("../ToFImaging/UnitTests/test_data/inimodel_Aexp.txt"); //opening the file. //path should be related to the lib
+    ifstream myfile_y2 ("../ToFImaging/UnitTests/test_data/logy.txt"); //opening the file. //path should be related to the lib
+
+
+    int loop_y=0;
+    for (double a; myfile_y>>a;)
+    {
+        first_guess[loop_y]=a;
+        loop_y++;
+
+    }
+
+    loop_y=0;
+    for (double a; myfile_y2>>a;)
+    {
+        y[loop_y]=a;
+        loop_y++;
+    }
+
+    QCOMPARE(loop_y, 1107);
+
+    BraggEdge::EdgeFunction myedge(7);
+    double *computed_firstedge = new double[N];
+    for (int i=0; i<N; ++i)
+    {
+        computed_firstedge[i] = BraggEdge::EdgeFunction::EdgeFunctionAExponential(x[i], param);
+//        qDebug() << computed_firstedge[i];
+//        qDebug() << first_guess[i];
+        QVERIFY(fabs(computed_firstedge[i]-first_guess[i])<eps); // compare the computed first edge with the loaded one, passed
+
+        }
+
+
+    edgefitting myfit(7, BraggEdge::eEdgeFunction::EdgeAttenuationExponential);
+    myfit.intialize_params(param);
+    myfit.fit(x,y,N);
+
+
+    double *updated_params = new double[7];
+    myfit.get_params(updated_params);
+
+    for (int i=0; i<7; ++i)
+    {
+        qDebug() << "expected: "  << expected_param[i] << ", computed: " << updated_params[i];
+//        QVERIFY(fabs(expected_param[i]-updated_params[i])<eps);
+    }
+
+
+
+}
+
+void ToFImagingAlgorithm::test_AttenuationLin()
+{
+
+    double *param = new double[7]; // initial parameters
+    param[0]=0.056568;
+    param[1]=0.0001;
+    param[2]=0.0015;
+    param[3]=0.3221873247378136;
+    param[4]=5.2674654703300545;
+    param[5]=-0.12174127292834003;
+    param[6]=31.65784345829804;
+
+
+    double *expected_params = new double[7];
+    expected_params[0] = 0.05788519;
+    expected_params[1] = 2.1650e-04;
+    expected_params[2] = 1.5581e-04;
+    expected_params[3] = 0.14975358;
+    expected_params[4] = 7.67236371;
+    expected_params[5] = 0.15162227;
+    expected_params[6] = 26.4337443;
+
+    short loop=0; //short for loop for input
+    string line; //this will contain the data read from the file
+
+    ifstream myfile("../ToFImaging/UnitTests/test_data/x.txt"); //opening the file.
+
+
+    unsigned int N=1107;
+    double *x = new double[N];
+    double *first_guess = new double[N];
+    double *y = new double[N];
+
+    double eps=0.001;
 
     for (double a; myfile>>a;)
     {
