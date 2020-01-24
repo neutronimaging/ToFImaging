486df573145b7cf245e7045ca20d81bf53ed968e
chiaracarminati <chiara.carminati@psi.ch>
2020-01-24 17:58:00 +0100

    first steps for TOF_Calibration notebook. work in progress X

--- scripts/TOF_routines.py
+++ scripts/TOF_routines.py
@@ -38,11 +38,10 @@ def find_last(array, value):
             if ((array[idx]-array[idx-1])>=value+0.005):
                 break
     return idx
-#------------------Monica-----
+
 def rotatedata(x,y,a):
     cosa=np.cos(a)
     sina=np.sin(a)
     x = x*cosa-y*sina
     y = x*sina+y*cosa
     return x,y
-#---------------------------------
