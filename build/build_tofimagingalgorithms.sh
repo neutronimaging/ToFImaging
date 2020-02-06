85bb5e653f85b3df18f81d0b34a7c0591b84ab61
chiaracarminati <chiara.carminati@psi.ch>
2020-02-06 11:59:16 +0100

    some cleaning

--- build/build_tofimagingalgorithms.sh
+++ build/build_tofimagingalgorithms.sh
@@ -11,15 +11,13 @@ DEST_DEPENDENCY=$WORKSPACE/imagingsuite/external/src/lmfit/lmfit_install
 REPOSPATH=$WORKSPACE/ToFImaging
 DEST=$WORKSPACE/builds
 
-
-
 mkdir $WORKSPACE/imagingsuite/external/src/lmfit/build
 mkdir $DEST_DEPENDENCY
 cd $WORKSPACE/imagingsuite/external/src/lmfit/build
 
 $CMAKEBINPATH/cmake -DCMAKE_INSTALL_PREFIX=$DEST_DEPENDENCY .. 
 make
-make install 
+make install
 cp $DEST_DEPENDENCY/lib/* $WORKSPACE/lib
 
 mkdir -p $DEST/build-tofimagingalgorithm
