76c411834fffa64d40db07bc5b060c00c2ccadf9
Matteo <basebush@gmail.com>
2020-09-29 10:53:06 +0200

    Testing function listing

--- python_modules/phasefitting_1D.py
+++ python_modules/phasefitting_1D.py
@@ -9,19 +9,26 @@ from reduction_tools import find_nearest
 from reduction_tools import savitzky_golay as SG_filter
 
 def phase_ratio_linearcomb(lac,spectrum,phase1lac,phase2lac,phase_spectrum,lambda_range_norm,lambda_range_fit,est_phi=0.5,method ='least_squares',bool_SG=False,SG_w=5,SG_n=1,bool_print=0): 
-    ##INPUTS:
-    #lac: measured LAC spectrum to fit
-    #spectrum: lambda spectrum of the lac
-    #phase1lac: LAC of the 1st phase to fit
-    #phase2lac: LAC of the 2nd phase to fit
-    #phase_spectrum: lambda spectrum of the theoretical phases LAC
-    #lambda_range_norm: lambda range used to normalize data
-    #lambda_range_fit: lambda range where to perform the fitting
-    #est_phi: estimated phase fraction
-    #bool_plot: flag to activate plotting
+    """ Performs phase ratio fitting on linear combination of two basis functions, works with linear attenuation coefficient (LAC) spectra
+    INPUTS:
+    lac = 1d array the attenuation -log(I/I0) TOF images (x,y,lambda) 
+    spectrum = spectrum, length of this ndarray must correspond to size of lac_tof(lambda)
+    phase1lac = lac of the phase 1
+    phase2lac = lac of the phase 2
+    spectrum_phase = spectrum corresponding to phase 1 and 2
+    lambda_range_norm = lambda range where to normalize spectra
+    lambda_range_edges = lambda range where to do the fitting
+    est_phi = estimate phase 1 weight
+    method = fitting method
+    bool_SG = set to True to perform Savitzky-Golay filtering of the transmission derivative
+    SG_w = window size of S-G filter
+    SG_n = order of S-G filter
+    bool_print = set to True to print output
 
-    ##OUTPUTS:
-    #phi: fitted phase fraction
+    OUTPUTS:
+    #dictionary with the following fit in the dimension of the mask
+    'phi' : phase1 weight
+    """ 
 
     if(bool_SG):
         lac = SG_filter(lac,SG_w,SG_n)
@@ -86,19 +93,31 @@ def phase_ratio_linearcomb(lac,spectrum,phase1lac,phase2lac,phase_spectrum,lambd
     return {'phi': phi}
 
 def phase_ratio_linearcomb_three(lac,spectrum,phase1lac,phase2lac,phase3lac,phase_spectrum,lambda_range_norm,lambda_range_fit,est_f1=0.333,est_f2=0.333,est_f3=0.334,method ='least_squares',bool_SG=False,SG_w=5,SG_n=1,bool_print=0):     
-    ##INPUTS:
-    #lac: measured LAC spectrum to fit
-    #spectrum: lambda spectrum of the lac
-    #phase1lac: LAC of the 1st phase to fit
-    #phase2lac: LAC of the 2nd phase to fit
-    #phase_spectrum: lambda spectrum of the theoretical phases LAC
-    #lambda_range_norm: lambda range used to normalize data
-    #lambda_range_fit: lambda range where to perform the fitting
-    #est_phi: estimated phase fraction
-    #bool_plot: flag to activate plotting
+    """ Performs phase ratio fitting on linear combination of two basis functions, works with linear attenuation coefficient (LAC) spectra
+    INPUTS:
+    lac = 1d array the attenuation -log(I/I0) TOF images (x,y,lambda) 
+    spectrum = spectrum, length of this ndarray must correspond to size of lac_tof(lambda)
+    phase1lac = lac of the phase 1
+    phase2lac = lac of the phase 2
+    phase3lac = lac of the phase 3
+    spectrum_phase = spectrum corresponding to phase 1 and 2
+    lambda_range_norm = lambda range where to normalize spectra
+    lambda_range_edges = lambda range where to do the fitting
+    est_f1 = estimate phase 1 weight
+    est_f2 = estimate phase 2 weight
+    est_f3 = estimate phase 3 weight
+    method = fitting method
+    bool_SG = set to True to perform Savitzky-Golay filtering of the transmission derivative
+    SG_w = window size of S-G filter
+    SG_n = order of S-G filter
+    bool_print = set to True to print output
 
-    ##OUTPUTS:
-    #phi: fitted phase fraction
+    OUTPUTS:
+    #dictionary with the following fit in the dimension of the mask
+    'phi1' : phase1 weight
+    'phi2' : phase2 weight
+    'phi3' : phase3 weight
+    """ 
     
     if(bool_SG):
         lac = SG_filter(lac,SG_w,SG_n)
