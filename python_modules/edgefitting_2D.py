7975ecfc7f67321d2234ceda810fdc98c573003c
Matteo <basebush@gmail.com>
2020-09-29 10:44:19 +0200

    Small bugs catched and calibration matrix functions added

--- python_modules/edgefitting_2D.py
+++ python_modules/edgefitting_2D.py
@@ -16,23 +16,21 @@ import time
    
 def AdvancedBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]),auto_mask=True,mask_thresh=[0.05, 0.95],est_pos=0,est_sigma=1,est_alpha=1,bool_smooth=True,smooth_w=5,smooth_n=1,bool_linear=False,bool_save=False,bool_print=False,debug_idx=[]):            
     """ Performs edge fitting with gaussian model to stack of TOF images (x,y,lambda)
-    
     INPUTS:
     Ttof = 3d matrix with the stack of TRANSMISSION (I/I0) TOF images (x,y,lambda) 
     spectrum = spectrum, length of this ndarray must correspond to size of Ttof(lambda)
     spectrum_range = range corresponding to lambda where to perform the fitting
-    filemask = mask of where to perform the fit (x,y)
+    mask = mask of where to perform the fit (x,y)
     auto_mask = if True will automatically mask the region based on the mask_thresh thresholds
     mask_thresh = low and high threshold for the automatic mask
     est_pos = estimated bragg edge position (in spectrum dimension)
-    est_wid = estimated bragg edge width (in spectrum dimension)
-    est_h = estimated bragg edge height (in spectrum dimension)
+    est_sigma = expected Gaussian broadening
+    est_alpha = expected decay constant (moderator property)
     bool_smooth = set to True to perform Savitzky-Golay filtering of the transmission derivative
     smooth_w = window size of S-G filter
     smooth_n = order of S-G filter
     bool_save = set to True to save output
     bool_print = set to True to print output
-    debug_flag = set to True to test on single pixel (will actually also perform the whole image fitting if not stopped)
     debug_idx = pixel coordinates where to test the single pixel fitting
 
     OUTPUTS:
@@ -43,6 +41,7 @@ def AdvancedBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
     'edge_slope': edge slope 
     'median_image': median Transmission image in the selected lambda range
     """ 
+
     if(mask.any()):
         if( [np.shape(Ttof)[0], np.shape(Ttof)[1]] != [np.shape(mymask)[0], np.shape(mymask)[1]]):
             print('WARNING: Mask size does not match frames size')
@@ -72,8 +71,6 @@ def AdvancedBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
     myrange = []
     myrange.append(find_nearest(spectrum, spectrum_range[0])) # 3.7
     myrange.append(find_nearest(spectrum, spectrum_range[1])) # 4.4
-    if(est_pos):
-        est_pos = find_nearest(spectrum[myrange[0]:myrange[1]], est_pos) # 4.05
     if(any(debug_idx)): #testing on a single pixel  
         signal = Ttof[debug_idx[0],debug_idx[1],:]
         edgefitting_1D.AdvancedBraggEdgeFitting(signal=signal,spectrum=spectrum,spectrum_range=myrange,est_pos=est_pos,est_sigma=est_sigma,est_alpha=est_alpha,bool_smooth=bool_smooth,smooth_w=smooth_w,smooth_n=smooth_n,bool_linear=bool_linear,bool_print=True)
@@ -83,9 +80,9 @@ def AdvancedBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
     edge_position = np.zeros(np.shape(mymask))
     edge_width = np.zeros(np.shape(mymask))
     edge_height = np.zeros(np.shape(mymask))
-    #loop for all pixel position, where the mask is equal to one
+    
     start_time = time.time()
-    for i in tqdm(range(0, np.shape(mymask)[0])):
+    for i in tqdm(range(0, np.shape(mymask)[0])): #loop for all pixel position, where the mask is equal to one
         # print('---------------$$$$---------------')
         # print('Processing row n. ', i, 'of', np.shape(mymask)[0])
         for j in range(0, np.shape(mymask)[1]):
@@ -121,23 +118,23 @@ def AdvancedBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
 
 def AdvancedBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,spectrum_range,mask=np.ndarray([0]),auto_mask=True,mask_thresh=[0.05, 0.95],est_pos=0,est_sigma=1,est_alpha=1,bool_smooth=True,smooth_w=5,smooth_n=1,bool_linear=False,bool_save=False,bool_print=False,debug_idx=[]):            
     """ Performs edge fitting with gaussian model to stack of TOF images (x,y,lambda)
-    
     INPUTS:
     Ttof = 3d matrix with the stack of TRANSMISSION (I/I0) TOF images (x,y,lambda) 
     spectrum = spectrum, length of this ndarray must correspond to size of Ttof(lambda)
+    calibration_matrix = calibration matrix with the coefficients to convert from spectrum to lambda size (x,y,[X0,k]);
+                         will convert to lambda using formula Y = X0 + kX where X is spectrum for each pixel (x,y)
     spectrum_range = range corresponding to lambda where to perform the fitting
-    filemask = mask of where to perform the fit (x,y)
+    mask = mask of where to perform the fit (x,y)
     auto_mask = if True will automatically mask the region based on the mask_thresh thresholds
     mask_thresh = low and high threshold for the automatic mask
     est_pos = estimated bragg edge position (in spectrum dimension)
-    est_wid = estimated bragg edge width (in spectrum dimension)
-    est_h = estimated bragg edge height (in spectrum dimension)
+    est_sigma = expected Gaussian broadening
+    est_alpha = expected decay constant (moderator property)
     bool_smooth = set to True to perform Savitzky-Golay filtering of the transmission derivative
     smooth_w = window size of S-G filter
     smooth_n = order of S-G filter
     bool_save = set to True to save output
     bool_print = set to True to print output
-    debug_flag = set to True to test on single pixel (will actually also perform the whole image fitting if not stopped)
     debug_idx = pixel coordinates where to test the single pixel fitting
 
     OUTPUTS:
@@ -148,6 +145,7 @@ def AdvancedBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,sp
     'edge_slope': edge slope 
     'median_image': median Transmission image in the selected lambda range
     """ 
+
     if(mask.any()):
         mymask = mask
         plt.figure()
@@ -173,30 +171,25 @@ def AdvancedBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,sp
     else:
         mymask = np.ones([np.shape(Ttof)[0], np.shape(Ttof)[1]])
 
-    myrange = []
-    myrange.append(find_nearest(spectrum, spectrum_range[0])) # 3.7
-    myrange.append(find_nearest(spectrum, spectrum_range[1])) # 4.4
-    if(est_pos):
-        est_pos = find_nearest(spectrum[myrange[0]:myrange[1]], est_pos) # 4.05
+    if ((np.shape(Ttof)[0]!=np.shape(calibration_matrix)[0]) | (np.shape(Ttof)[1]!=np.shape(calibration_matrix)[1])):
+        print('!!!!!!! WARNING CALIBRATION MATRIX HAS NOT SAME SIZE OF IMAGE !!!!!!!!!!!!!!')
+
     if(any(debug_idx)): #testing on a single pixel  
         lambd = reduction_tools.tof2l_t0k(spectrum,calibration_matrix[debug_idx[0],debug_idx[1],1],calibration_matrix[debug_idx[0],debug_idx[1],0])
         myrange = []
         myrange.append(find_nearest(lambd, spectrum_range[0])) 
         myrange.append(find_nearest(lambd, spectrum_range[1]))
         signal = Ttof[debug_idx[0],debug_idx[1],:]
-        try:
-            edgefitting_1D.AdvancedBraggEdgeFitting(signal=signal,spectrum=spectrum,spectrum_range=myrange,est_pos=est_pos,est_sigma=est_sigma,est_alpha=est_alpha,bool_smooth=bool_smooth,smooth_w=smooth_w,smooth_n=smooth_n,bool_linear=bool_linear,bool_print=True)
-        except:
-            print('Fitting failed')
+        edgefitting_1D.AdvancedBraggEdgeFitting(signal=signal,spectrum=spectrum,spectrum_range=myrange,est_pos=est_pos,est_sigma=est_sigma,est_alpha=est_alpha,bool_smooth=bool_smooth,smooth_w=smooth_w,smooth_n=smooth_n,bool_linear=bool_linear,bool_print=True)
         return
 
     median_image = reduction_tools.medianimage(Ttof)
     edge_position = np.zeros(np.shape(mymask))
     edge_width = np.zeros(np.shape(mymask))
     edge_height = np.zeros(np.shape(mymask))
-    #loop for all pixel position, where the mask is equal to one
+    
     start_time = time.time()
-    for i in tqdm(range(0, np.shape(mymask)[0])):
+    for i in tqdm(range(0, np.shape(mymask)[0])): #loop for all pixel position, where the mask is equal to one
         # print('---------------$$$$---------------')
         # print('Processing row n. ', i, 'of', np.shape(mymask)[0])
         for j in range(0, np.shape(mymask)[1]):
@@ -236,12 +229,11 @@ def AdvancedBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,sp
 
 def GaussianBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]),auto_mask=True,mask_thresh=[0.05, 0.95],est_pos=0,est_wid=0,est_h=0,pos_BC=0,wid_BC=0,h_BC=0,bool_log=False,bool_smooth=True,smooth_w=5,smooth_n=1,bool_save=False,bool_print=False,debug_idx=[]):        
     """ Performs edge fitting with gaussian model to stack of TOF images (x,y,lambda)
-    
     INPUTS:
     Ttof = 3d matrix with the stack of TRANSMISSION (I/I0) TOF images (x,y,lambda) 
     spectrum = spectrum, length of this ndarray must correspond to size of Ttof(lambda)
     spectrum_range = range corresponding to lambda where to perform the fitting
-    filemask = mask of where to perform the fit (x,y)
+    mask = mask of where to perform the fit (x,y)
     auto_mask = if True will automatically mask the region based on the mask_thresh thresholds
     mask_thresh = low and high threshold for the automatic mask
     est_pos = estimated bragg edge position (in spectrum dimension)
@@ -250,13 +242,12 @@ def GaussianBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
     pos_BC = boundary conditions for the bragg edge position fit
     wid_BC = boundary conditions for the bragg edge width fit
     h_BC = boundary conditions for the bragg edge height fit
+    bool_log = set to True to convert T to mu
     bool_smooth = set to True to perform Savitzky-Golay filtering of the transmission derivative
     smooth_w = window size of S-G filter
     smooth_n = order of S-G filter
-    bool_log = set to True to convert T to mu
     bool_save = set to True to save output
     bool_print = set to True to print output
-    debug_flag = set to True to test on single pixel (will actually also perform the whole image fitting if not stopped)
     debug_idx = pixel coordinates where to test the single pixel fitting
 
     OUTPUTS:
@@ -267,6 +258,7 @@ def GaussianBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
     'edge_slope': edge slope 
     'median_image': median Transmission image in the selected lambda range
     """ 
+
     if(mask.any()):
         mymask = mask
         plt.figure()
@@ -300,10 +292,7 @@ def GaussianBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
     myrange.append(find_nearest(spectrum, spectrum_range[1])) # 4.4
     if(any(debug_idx)): #testing on a single pixel    
         sp = Ttof[debug_idx[0],debug_idx[1],:]
-        try:
-            edgefitting_1D.GaussianBraggEdgeFitting(signal=sp,spectrum=spectrum,spectrum_range=myrange,est_pos=est_pos,est_wid=est_wid,est_h=est_h,pos_BC=pos_BC,wid_BC=wid_BC,h_BC=h_BC,bool_log=bool_log,bool_smooth=bool_smooth,smooth_w=smooth_w,smooth_n=smooth_n,bool_print=debug_flag)
-        except:
-            print('Fitting failed')
+        edgefitting_1D.GaussianBraggEdgeFitting(signal=sp,spectrum=spectrum,spectrum_range=myrange,est_pos=est_pos,est_wid=est_wid,est_h=est_h,pos_BC=pos_BC,wid_BC=wid_BC,h_BC=h_BC,bool_log=bool_log,bool_smooth=bool_smooth,smooth_w=smooth_w,smooth_n=smooth_n,bool_print=True)
         return
 
     median_image = reduction_tools.medianimage(Ttof)
@@ -311,9 +300,9 @@ def GaussianBraggEdgeFitting_2D(Ttof,spectrum,spectrum_range,mask=np.ndarray([0]
     edge_width = np.zeros(np.shape(mymask))
     edge_height = np.zeros(np.shape(mymask))
     edge_slope = np.zeros(np.shape(mymask))
-    #loop for all pixel position, where the mask is equal to one
+    
     start_time = time.time()
-    for i in tqdm(range(0, np.shape(mymask)[0])):
+    for i in tqdm(range(0, np.shape(mymask)[0])): #loop for all pixel position, where the mask is equal to one
         # print('---------------$$$$---------------')
         # print('Processing row n. ', i, 'of', np.shape(mymask)[0])
         for j in range(0, np.shape(mymask)[1]):
@@ -351,12 +340,12 @@ def GaussianBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,sp
     """ Performs edge fitting with gaussian model to stack of TOF images (x,y,lambda)
     
     INPUTS:
-    Ttof = 3d matrix with the stack of TRANSMISSION (I/I0) TOF images (x,y,spectrum) 
+    Ttof = 3d matrix with the stack of TRANSMISSION (I/I0) TOF images (x,y,lambda) 
     spectrum = spectrum, length of this ndarray must correspond to size of Ttof(lambda)
     calibration_matrix = calibration matrix with the coefficients to convert from spectrum to lambda size (x,y,[X0,k]);
                          will convert to lambda using formula Y = X0 + kX where X is spectrum for each pixel (x,y)
     spectrum_range = range corresponding to lambda where to perform the fitting
-    filemask = mask of where to perform the fit (x,y)
+    mask = mask of where to perform the fit (x,y)
     auto_mask = if True will automatically mask the region based on the mask_thresh thresholds
     mask_thresh = low and high threshold for the automatic mask
     est_pos = estimated bragg edge position (in spectrum dimension)
@@ -365,12 +354,12 @@ def GaussianBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,sp
     pos_BC = boundary conditions for the bragg edge position fit
     wid_BC = boundary conditions for the bragg edge width fit
     h_BC = boundary conditions for the bragg edge height fit
+    bool_log = set to True to convert T to mu
     bool_smooth = set to True to perform Savitzky-Golay filtering of the transmission derivative
     smooth_w = window size of S-G filter
     smooth_n = order of S-G filter
     bool_save = set to True to save output
     bool_print = set to True to print output
-    debug_flag = set to True to test on single pixel (will actually also perform the whole image fitting if not stopped)
     debug_idx = pixel coordinates where to test the single pixel fitting
 
     OUTPUTS:
@@ -412,16 +401,14 @@ def GaussianBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,sp
 
     if ((np.shape(Ttof)[0]!=np.shape(calibration_matrix)[0]) | (np.shape(Ttof)[1]!=np.shape(calibration_matrix)[1])):
         print('!!!!!!! WARNING CALIBRATION MATRIX HAS NOT SAME SIZE OF IMAGE !!!!!!!!!!!!!!')
+
     if(any(debug_idx)): #testing on a single pixel    
         lambd = reduction_tools.tof2l_t0k(spectrum,calibration_matrix[debug_idx[0],debug_idx[1],1],calibration_matrix[debug_idx[0],debug_idx[1],0])
         myrange = []
         myrange.append(find_nearest(lambd, spectrum_range[0])) 
         myrange.append(find_nearest(lambd, spectrum_range[1]))
         signal = Ttof[debug_idx[0],debug_idx[1],:]
-        try:
-            edgefitting_1D.GaussianBraggEdgeFitting(signal=signal,spectrum=lambd,spectrum_range=myrange,est_pos=est_pos,est_wid=est_wid,est_h=est_h,pos_BC=pos_BC,wid_BC=wid_BC,h_BC=h_BC,bool_log=bool_log,bool_smooth=bool_smooth,smooth_w=smooth_w,smooth_n=smooth_n,bool_print=debug_flag)
-        except:
-            print('Fitting failed')
+        edgefitting_1D.GaussianBraggEdgeFitting(signal=signal,spectrum=lambd,spectrum_range=myrange,est_pos=est_pos,est_wid=est_wid,est_h=est_h,pos_BC=pos_BC,wid_BC=wid_BC,h_BC=h_BC,bool_log=bool_log,bool_smooth=bool_smooth,smooth_w=smooth_w,smooth_n=smooth_n,bool_print=True)
         return
 
     median_image = reduction_tools.medianimage(Ttof)
@@ -429,9 +416,9 @@ def GaussianBraggEdgeFitting_2D_Calib_matrix(Ttof,spectrum,calibration_matrix,sp
     edge_width = np.zeros(np.shape(mymask))
     edge_height = np.zeros(np.shape(mymask))
     edge_slope = np.zeros(np.shape(mymask))
-    #loop for all pixel position, where the mask is equal to one
+    
     start_time = time.time()
-    for i in tqdm(range(0, np.shape(mymask)[0])):
+    for i in tqdm(range(0, np.shape(mymask)[0])): #loop for all pixel position, where the mask is equal to one
         # print('---------------$$$$---------------')
         # print('Processing row n. ', i, 'of', np.shape(mymask)[0])
         for j in range(0, np.shape(mymask)[1]):
