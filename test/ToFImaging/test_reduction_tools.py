6f8c6b1cba48037fae4f2f9eb31bc9d0157719c9
JeanBilheux <bilheuxjm@ornl.gov>
2021-04-15 13:05:07 -0400

    fixed first unit test. this refs #71

--- test/ToFImaging/test_reduction_tools.py
+++ test/ToFImaging/test_reduction_tools.py
@@ -191,13 +191,24 @@ class TestMovingAverage2D:
             reduction_tools.moving_average_2D(input_array=input_array)
 
     def test_using_custom_kernel(self):
-        input_array = np.array([[1, 10, 11], [9, 2, 1], [1, 30, 15]])
+        input_array = np.array([[[1, 10, 11], [9, 2, 1], [1, 30, 15]],
+                               [[1, 10, 11], [9, 2, 1], [1, 30, 15]],
+                               [[1, 10, 11], [9, 2, 1], [1, 30, 15]],
+                               [[1, 10, 11], [9, 2, 1], [1, 30, 15]]])
         output_array = reduction_tools.moving_average_2D(input_array=input_array,
-                                                         custom_kernel=np.ones((3, 3)))
-        expected_array = np.array([[(1 + 10 + 9 + 2) / 9, (1 + 10 + 11 + 9 + 2 + 1) / 9, (10 + 11 + 2 + 1) / 9],
-                                   [(1 + 10 + 9 + 2 + 1 + 30) / 9, (1 + 10 + 11 + 9 + 2 + 1 + 1 + 30 + 15) / 9,
-                                    (10 + 11 + 2 + 1 + 30 + 15) / 9],
-                                   [(9 + 2 + 1 + 30) / 9, (9 + 2 + 1 + 1 + 30 + 15) / 9, (2 + 1 + 30 + 15) / 9]])
+                                                         custom_kernel=np.ones((2, 2)))
+        expected_array = [[[0.25, 2.75, 5.25],
+                           [2.5, 5.5, 6],
+                           [2.5, 10.5, 12]],
+                          [[0.25, 2.75, 5.25],
+                           [2.5, 5.5, 6.],
+                           [2.5, 10.5, 12.]],
+                          [[0.25, 2.75, 5.25],
+                           [2.5, 5.5, 6],
+                           [2.5, 10.5, 12]],
+                          [[0.25, 2.75, 5.25],
+                           [2.5, 5.5, 6],
+                           [2.5, 10.5, 12.]]]
         assert np.shape(input_array) == np.shape(output_array)
         for _output_row, _expected_row in zip(output_array, expected_array):
             for _output, _expected in zip(_output_row, _expected_row):
