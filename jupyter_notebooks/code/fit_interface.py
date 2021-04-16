97ae8b5e910a327ca0455f931a1e9c1fd4573197
JeanBilheux <bilheuxjm@ornl.gov>
2021-04-16 15:53:45 -0400

    Moving settings tab step3 and 4 to their own files. this refs #82

--- jupyter_notebooks/code/fit_interface.py
+++ jupyter_notebooks/code/fit_interface.py
@@ -1,15 +1,11 @@
 import os
 from qtpy.QtWidgets import QMainWindow
-from qtpy import QtGui
 from jupyter_notebooks.code import load_ui
 import pyqtgraph as pg
 import numpy as np
 import copy
 import inflect
 
-from NeuNorm.normalization import Normalization
-
-from ToFImaging import reduction_tools
 from jupyter_notebooks.code.decorators import wait_cursor
 from jupyter_notebooks.code.fit_handler import FitHandler
 from jupyter_notebooks.code.utilities.get import Get
@@ -18,6 +14,8 @@ from jupyter_notebooks.code.roi_selection import Interface as RoiInterface
 from jupyter_notebooks.code.export import Export
 from jupyter_notebooks.code.event_handler import EventHandler
 from jupyter_notebooks.code.prepare_data import PrepareData
+from jupyter_notebooks.code.step3_settings_handler import Step3SettingsHandler
+from jupyter_notebooks.code.step4_settings_handler import Step4SettingsHandler
 
 MARKER_HEIGHT, MARKER_WIDTH = 20, 20
 COLOR_LAMBDA_RANGE = [250, 128, 247]
@@ -131,15 +129,6 @@ class Interface(QMainWindow):
     def prepare_data_button_clicked(self):
         o_event = PrepareData(parent=self)
         o_event.prepare_data()
-
-    # def mcp_correction(self):
-    #     if self.is_mcp_correction_requested:
-    #         self.ui.statusbar.showMessage("MCP correction ... processing!")
-
-
-
-
-
 
     def activate_moving_average_clicked(self):
         status = self.ui.activate_moving_average_checkBox.isChecked()
@@ -506,137 +495,6 @@ class Interface(QMainWindow):
             self.step4_settings_ui = step4_settings_ui
 
     def apply_clicked(self):
-        self.close()
-
-
-class Step3SettingsHandler(QMainWindow):
-
-    def __init__(self, parent=None):
-        super(QMainWindow, self).__init__()
-        self.parent = parent
-        ui_full_path = os.path.join(os.path.dirname(__file__), os.path.join('ui', 'ui_step3_settings.ui'))
-        self.ui = load_ui(ui_full_path, baseinstance=self)
-        self.setWindowTitle("Step3 - Settings")
-        self.init_widgets()
-
-    def init_widgets(self):
-        if self.parent.debugging_mode:
-            bragg_peak_estimated = "3.5"
-        else:
-            bragg_peak_estimated = self.parent.o_roi.lambda_array[
-                self.parent.ui.rough_lambda_peak_position_slider.value()]
-        self.ui.estimated_position_bragg_edge_label.setText("{:.3f}".format(bragg_peak_estimated))
-        self.ui.estimated_units_label.setText(u"\u212B")
-
-        config = self.parent.step3_config
-        self.ui.cross_section_mode_radioButton.setChecked(config['is_cross_section_mode'])
-        self.ui.automatic_masking_checkBox.setChecked(config['is_automatic_masking'])
-        self.ui.low_threshold_lineEdit.setText(str(config['threshold_low']))
-        self.ui.high_threshold_lineEdit.setText(str(config['threshold_high']))
-        self.ui.perform_filtering_algorithm_checkBox.setChecked(config['is_perform_savitzky_golay_filtering'])
-        self.ui.window_size_lineEdit.setText(str(config['window_size']))
-        self.ui.order_number_lineEdit.setText(str(config['order_number']))
-        self.ui.interpolation_factor_checkBox.setChecked(config['is_interpolation_factor'])
-        self.ui.interpolation_factor_lineEdit.setText(str(config['interpolation_factor_value']))
-
-        self.automatic_masking_clicked(config['is_automatic_masking'])
-        self.perform_filtering_algorithm_clicked(config['is_perform_savitzky_golay_filtering'])
-        self.interpolation_factor_clicked(config['is_interpolation_factor'])
-
-    def automatic_masking_clicked(self, status):
-        self.ui.threshold_groupBox.setEnabled(status)
-
-    def perform_filtering_algorithm_clicked(self, status):
-        self.ui.perform_filtering_algorithm_groupBox.setEnabled(status)
-
-    def interpolation_factor_clicked(self, status):
-        self.ui.interpolation_factor_lineEdit.setEnabled(status)
-
-    def save_settings(self):
-        step3_config = {'is_automatic_masking': self.ui.automatic_masking_checkBox.isChecked(),
-                        'threshold_low': str(self.ui.low_threshold_lineEdit.text()),
-                        'threshold_high': str(self.ui.high_threshold_lineEdit.text()),
-                        'is_perform_savitzky_golay_filtering':
-                            self.ui.perform_filtering_algorithm_checkBox.isChecked(),
-                        'window_size': str(self.ui.window_size_lineEdit.text()),
-                        'order_number': str(self.ui.order_number_lineEdit.text()),
-                        'is_interpolation_factor': self.ui.interpolation_factor_checkBox.isChecked(),
-                        'interpolation_factor_value': str(self.ui.interpolation_factor_lineEdit),
-                        'is_cross_section_mode': self.ui.cross_section_mode_radioButton.isChecked()}
-        self.parent.step3_config = step3_config
-
-    def validate_changes_clicked(self):
-        self.save_settings()
-        self.close()
-
-    def closeEvent(self, event=None):
-        self.parent.step3_settings_ui = None
-        self.close()
-
-class Step4SettingsHandler(QMainWindow):
-
-    def __init__(self, parent=None):
-        super(QMainWindow, self).__init__()
-        self.parent = parent
-        ui_full_path = os.path.join(os.path.dirname(__file__), os.path.join('ui', 'ui_step4_settings.ui'))
-        self.ui = load_ui(ui_full_path, baseinstance=self)
-        self.setWindowTitle("Step4 - Settings")
-        self.init_widgets()
-
-    def init_widgets(self):
-        config = self.parent.step4_config
-
-        self.ui.estimated_bragg_edge_position_lineEdit.setText("{.2f}".format(config[
-                                                                                  'estimated_bragg_edge_position_value']))
-        self.ui.estimated_bragg_edge_width_lineEdit.setText("{.2f}".format(config['estimated_bragg_edge_width_value']))
-        self.ui.estimated_bragg_edge_height_lineEdit.setText("{.2f}".format(config[
-                                                                                'estimated_bragg_edge_height_value']))
-
-        position_from = config['estimated_bragg_edge_position_range'][0]
-        position_to = config['estimated_bragg_edge_position_range'][1]
-        self.ui.boundary_position_from_lineEdit.setText("{:.2f}".format(position_from))
-        self.ui.boundary_position_to_lineEdit.setText("{:.2f}".format(position_to))
-
-        width_from = config['estimated_bragg_edge_width_range'][0]
-        width_to = config['estimated_bragg_edge_width_range'][1]
-        self.ui.boundary_width_from_lineEdit.setText("{:.2f}".format(width_from))
-        self.ui.boundary_width_to_lineEdit.setText("{:.2f}".format(width_to))
-
-        self.ui.cross_section_mode_radioButton.setChecked(config['is_cross_section_mode'])
-
-        self.ui.automatic_masking_checkBox.setChecked(config['is_automatic_masking'])
-        self.ui.low_threshold_lineEdit.setText(str(config['threshold_low']))
-        self.ui.high_threshold_lineEdit.setText(str(config['threshold_high']))
-
-        self.ui.perform_filtering_algorithm_checkBox.setChecked(config['is_perform_savitzky_golay_filtering'])
-        self.ui.window_size_lineEdit.setText(str(config['window_size']))
-        self.ui.order_number_lineEdit.setText(str(config['order_number']))
-
-        self.ui.interpolation_factor_checkBox.setChecked(config['is_interpolation_factor'])
-        self.ui.interpolation_factor_lineEdit.setText(str(config['interpolation_factor_value']))
-
-        self.automatic_masking_clicked(config['is_automatic_masking'])
-        self.perform_filtering_algorithm_clicked(config['is_perform_savitzky_golay_filtering'])
-        self.interpolation_factor_clicked(config['is_interpolation_factor'])
-
-    def save_settings(self):
-        pass
-
-    def validate_changes_clicked(self):
-        self.save_settings()
-        self.close()
-
-    def automatic_masking_clicked(self, status):
-        self.ui.threshold_groupBox.setEnabled(status)
-
-    def perform_filtering_algorithm_clicked(self, status):
-        self.ui.perform_filtering_algorithm_groupBox.setEnabled(status)
-
-    def interpolation_factor_clicked(self, status):
-        self.ui.interpolation_factor_lineEdit.setEnabled(status)
-
-    def closeEvent(self, event=None):
-        self.parent.step3_settings_ui = None
         self.close()
 
 
